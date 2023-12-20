24353cb615113dd92c2698cec1080129d9817323
Andre Goncalves <andre@andregoncalves.com>
2023-12-20 10:25:03 +0000

    CHANGE: updated deploy scripts

--- scripts/deploy_live.js
+++ scripts/deploy_live.js
@@ -6,18 +6,14 @@ async function main() {
 
     // Polygon addresses
     const authContractAddress = "0x9928D9e849317983760a61FC486696001f387C6E";
+    const mintableRegistryContractAddress = "0x64450DA938d06bE7EEc68E4Ead99FfF05D8Cebe7";
+    const authorizedSignerAddress = "0x89145000ADBeCe9D1FFB26F645dcb0883bc5c3d9";
 
-    const contractFactory = await hre.ethers.getContractFactory("SelfkeyDaoVoting");
-    const contract = await upgrades.deployProxy(contractFactory, [authContractAddress], { timeout: 500000 });
+    const contractFactory = await hre.ethers.getContractFactory("SelfkeyDaoSelfAirdrop");
+    const contract = await upgrades.deployProxy(contractFactory, [authContractAddress, authorizedSignerAddress, mintableRegistryContractAddress], { timeout: 500000 });
     await contract.deployed();
 
     console.log("Deployed contract address:", contract.address);
-
-    //const signer = "0x89145000ADBeCe9D1FFB26F645dcb0883bc5c3d9";
-    //console.log("Controller wallet address:", signer);
-    //await contract.changeAuthorizedSigner(signer);
-
-    // await contract.createProposal('Mint Self', true);
 
     // INFO: verify contract after deployment
     // npx hardhat verify --network polygon 0x945B096376aE425Fc1a2c9DE77a85b1895132815
